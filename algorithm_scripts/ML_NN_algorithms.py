68fefdb70e7fda1a1ad9825be4b56d82715f275b
Karen <karenlstengel@gmail.com>
2018-06-13 16:26:30 -0600

    same as previous

--- algorithm_scripts/ML_NN_algorithms.py
+++ algorithm_scripts/ML_NN_algorithms.py
@@ -53,6 +53,7 @@ from keras.optimizers import SGD, Adam
 from IPython.display import SVG
 from keras.utils.vis_utils import model_to_dot, plot_model
 import sklearn.metrics as skm
+import tensorflow as tf
 import pickle
 import os
 import datetime
@@ -188,10 +189,10 @@ def dnn(neuronLayer, drop, learnRate, momentum, decay,boolNest, iterations, trai
     file.write("iterations "+ str(iterations) + "\n")
 
     #initilaize model with Sequential()
+    print(train_data.shape)
     denseModel = Sequential()
     #add first layers
-    #fix the dimesions in pooling layer
-    denseModel.add(AveragePooling2D(pool_size = (32,32), input_shape = train_data.shape[:1])) # negin used this as the first layer. need to double check syntax
+    denseModel.add(AveragePooling2D(pool_size = (32,32), input_shape = train_data.shape[:3])) # negin used this as the first layer. need to double check syntax
     denseModel.add(Flatten())
 
     for layer in neuronLayer:
@@ -425,7 +426,7 @@ if __name__ == "__main__":
             for f in os.listdir(folder):
 
                 if not f.startswith('.'):
-                    #fix the rest of these
+
                     with open(folder + "/" + f + "/" + f + ".txt", 'rb') as file:
                         if f == 'X_train':
                             train_data = np.array(pickle.load(file))
@@ -445,6 +446,7 @@ if __name__ == "__main__":
     #print(train_data[0])
     np.asarray(train_data)
     print(train_data[0].shape)
+    print(train_data[15].shape)
 
     denseNN = dnn([16,16,2], 0.5, 0.0001, 0.99, 1e-4, True, 1,train_data, train_label,dev_data, dev_label, outputFile) #these are all negins values right now.
     #convNN = cnn([20,50,500,2], [5,5], [2,2], [1,1], [2,2], 0.01, 1000, train_data, train_label, dev_data, dev_label, outputDL) # these are the lenet values
