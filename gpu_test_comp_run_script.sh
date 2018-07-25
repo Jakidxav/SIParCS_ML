19388fc134d1c162409b97fc39c20ae17dbf265f
Karen <karenlstengel@gmail.com>
2018-07-25 10:42:40 -0600

    data file moving

---
+++ gpu_test_comp_run_script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#
+#BATCH -C caldera
+#SBATCH -A SCSG0002
+#SBATCH -J rnn30
+#
+#SBATCH -n 2
+#SBATCH --gres=gpu:2
+#SBATCH -t 06:00:00
+#SBATCH -p dav
+#SBATCH -e job_name.err.%J
+#SBATCH -o job_name.out.%J
+
+export TMPDIR=/glade/scratch/$USER/temp
+mkdir -p $TMPDIR
+
+source /glade/u/home/joshuadr/tensor_test/bin/activate
+module load cuda/9.0
+
+cd /glade/work/joshuadr/SIParCS_ML/algorithm_scripts/
+
+python3 time_accuracy_test.py
